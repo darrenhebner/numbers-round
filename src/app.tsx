9f8c64dc68dd590b5009e02d8342d2ea92dfa0f6
Darren Hebner <darrenwilliamhebner@gmail.com>
2025-09-07 18:28:44 -0400

    minimal styling

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,8 @@
 import { signal, computed, Signal, type ReadonlySignal } from "@preact/signals";
+import { Delete } from "lucide-react";
 
 interface Number {
   value: number;
-}
-
-interface Config {
-  target: number;
-  numbers: Number[];
 }
 
 type State =
@@ -23,13 +19,12 @@ type State =
   | {
       state: "done";
       answer: number;
+      target: number;
     };
 
 const stateSignal = signal<State>({
   state: "initial",
 });
-
-const config = signal<Config | null>(null);
 
 function Home() {
   function buildGame(numberOfLarge: number) {
@@ -81,7 +76,7 @@ function Home() {
               stack.push(b + a);
               break;
             }
-            case "*": {
+            case "×": {
               stack.push(b * a);
               break;
             }
@@ -89,7 +84,7 @@ function Home() {
               stack.push(b - a);
               break;
             }
-            case "/": {
+            case "÷": {
               stack.push(b / a);
               break;
             }
@@ -103,16 +98,45 @@ function Home() {
 
   return (
     <div>
-      <button onClick={() => buildGame(0)}>0</button>
-      <button onClick={() => buildGame(1)}>1</button>
-      <button onClick={() => buildGame(2)}>2</button>
-      <button onClick={() => buildGame(3)}>3</button>
-      <button onClick={() => buildGame(4)}>4</button>
+      <h1 className="text-3xl text-blue-500 mt-16 mb-4">The Numbers Round</h1>
+      <p className="mb-4">How many big numbers would you like?</p>
+      <div className="grid grid-cols-5 gap-4 w-full justify-between">
+        <button
+          className="bg-gray-200 text-gray-800 aspect-square rounded-full text-lg"
+          onClick={() => buildGame(0)}
+        >
+          0
+        </button>
+        <button
+          className="bg-gray-200 text-gray-800 aspect-square rounded-full text-lg"
+          onClick={() => buildGame(1)}
+        >
+          1
+        </button>
+        <button
+          className="bg-gray-200 text-gray-800 aspect-square rounded-full text-lg"
+          onClick={() => buildGame(2)}
+        >
+          2
+        </button>
+        <button
+          className="bg-gray-200 text-gray-800 aspect-square rounded-full text-lg"
+          onClick={() => buildGame(3)}
+        >
+          3
+        </button>
+        <button
+          className="bg-gray-200 text-gray-800 aspect-square rounded-full text-lg"
+          onClick={() => buildGame(4)}
+        >
+          4
+        </button>
+      </div>
     </div>
   );
 }
 
-type Input = Number | "+" | "-" | "*" | "/";
+type Input = Number | "+" | "-" | "×" | "÷";
 
 function Game({
   currentGame,
@@ -130,50 +154,82 @@ function Game({
   }
 
   return (
-    <div>
-      <div>
+    <div className="h-dvh flex flex-col">
+      <div className="flex-grow text-center text-blue-500 my-2">
+        {currentGame.target}
+      </div>
+      <div className="text-3xl text-gray-700 mb-4 text-end">
         {input.value
-          .map((value) => {
-            if (typeof value === "object") {
-              return value.value;
-            }
-
-            return value;
-          })
+          .map((value) => (typeof value === "object" ? value.value : value))
           .join(" ")}
       </div>
-      <div>
-        {currentGame.numbers
-          .filter((num) => !input.value.includes(num))
-          .map((num, i) => (
-            <button key={`${num.value}-${i}`} onClick={handleClick(num)}>
+      <div className="grid grid-cols-4 grid-row-3 gap-2 text-xl mb-4">
+        <button
+          className="bg-blue-200 text-blue-800 rounded-full aspect-square"
+          onClick={handleClick("+")}
+        >
+          +
+        </button>
+        <button
+          className="bg-blue-200 text-blue-800 rounded-full aspect-square"
+          onClick={handleClick("-")}
+        >
+          -
+        </button>
+        <button
+          className="bg-blue-200 text-blue-800 rounded-full aspect-square"
+          onClick={handleClick("×")}
+        >
+          ×
+        </button>
+        <button
+          className="bg-blue-200 text-blue-800 rounded-full aspect-square"
+          onClick={handleClick("÷")}
+        >
+          ÷
+        </button>
+
+        <div className="grid col-span-3 row-span-2 grid-cols-subgrid grid-rows-subgrid">
+          {currentGame.numbers.map((num, i) => (
+            <button
+              key={`${num.value}-${i}`}
+              className="bg-gray-200 text-gray-800 disabled:text-gray-400 rounded-full aspect-square disabled:ring-2 ring-gray-400"
+              disabled={input.value.includes(num)}
+              onClick={handleClick(num)}
+            >
               {num.value}
             </button>
           ))}
+        </div>
+
+        <button
+          className="bg-gray-200 text-gray-800 rounded-full aspect-square grid place-items-center"
+          onClick={() => {
+            const next = [...input.value];
+            next.pop();
+            input.value = next;
+          }}
+        >
+          <Delete />
+        </button>
+        <button
+          onClick={() => {
+            stateSignal.value = {
+              state: "done",
+              target: currentGame.target,
+              answer: answer.value ?? 0,
+            };
+          }}
+          className="bg-blue-700 text-blue-200 rounded-full aspect-square"
+        >
+          =
+        </button>
       </div>
-      <div>
-        <button onClick={handleClick("+")}>+</button>
-        <button onClick={handleClick("-")}>-</button>
-        <button onClick={handleClick("*")}>*</button>
-        <button onClick={handleClick("/")}>/</button>
-      </div>
-      <button
-        onClick={() => {
-          const next = [...input.value];
-          next.pop();
-          input.value = next;
-        }}
-      >
-        ⬅️
-      </button>
-      <div>Target: {currentGame.target}</div>
-      <div>Answer: {answer.value}</div>
-      <button onClick={() => (config.value = null)}>Reset</button>
     </div>
   );
 }
 
-export function App() {
+function Screen() {
   const state = stateSignal.value;
 
   switch (state.state) {
@@ -184,7 +240,34 @@ export function App() {
       return <Game currentGame={state} />;
     }
     case "done": {
-      return null;
+      return (
+        <div className="mt-16">
+          <p className="text-lg mb-4">
+            You managed to get {state.answer}.{" "}
+            {state.answer > state.target
+              ? state.answer - state.target
+              : state.target - state.answer}{" "}
+            away from the target of {state.target}
+          </p>
+
+          <button
+            className="bg-blue-500 text-blue-100 p-4 rounded-full"
+            onClick={() => {
+              stateSignal.value = { state: "initial" };
+            }}
+          >
+            Play again
+          </button>
+        </div>
+      );
     }
   }
+}
+
+export function App() {
+  return (
+    <div className="max-w-md mx-auto px-4 font-mono">
+      <Screen />
+    </div>
+  );
 }
