8a4b5a9bc6f21d9c3487ea9248b0dc27e3e552ec
Darren Hebner <darrenwilliamhebner@gmail.com>
2025-09-07 18:44:25 -0400

    set up pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,43 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact(), tailwindcss()],
+  plugins: [
+    preact(),
+    tailwindcss(),
+    VitePWA({
+      injectRegister: "auto",
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        name: "Numbers Round",
+        short_name: "Numbers Round",
+        description:
+          "Practice your maths skills with Numbers Round. Based on the gameshow Countdown.",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        orientation: "portrait",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
